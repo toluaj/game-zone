58ae5b2eea3a4b443b15d3e20dff20eec1bfe61b
Tolu Ajia <toluajia@gmail.com>
2023-05-24 22:43:04 +0100

    chore: build color scheme toggler component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
+import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/logo.png'
+import ColorSchemeToggler from './ColorSchemeToggler'
 
 const NavBar = () => {
   return (
-    <HStack>
-        <Image src={logo} boxSize={16} />
+    <HStack justifyContent='space-between' padding={'10px'}>
+      <Image src={logo} boxSize={16} />
+      <ColorSchemeToggler />
     </HStack>
   )
 }
