ac53155be7ed1b15b1a091598a5b09479cfdd873
Tolu Ajia <toluajia@gmail.com>
2023-05-25 00:18:44 +0100

    chore: crop all game images to same size

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import GameCriticScore from "./GameCriticScore";
+import getCroppedImageUrl from "../services/gameImageUrl";
 
 interface GameCardProps {
   game: Game;
@@ -10,7 +11,11 @@ interface GameCardProps {
 const GameCard = ({ game }: GameCardProps) => {
   return (
     <Card>
-      <Image src={game.background_image} borderRadius={10} overflow="hidden" />
+      <Image
+        src={getCroppedImageUrl(game.background_image)}
+        borderRadius={10}
+        overflow="hidden"
+      />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
         <HStack justifyContent={"space-between"}>
