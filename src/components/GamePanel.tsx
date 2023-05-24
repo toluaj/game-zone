c5fcc06a1c494b228155d3515b8885e0d608a3ac
Tolu Ajia <toluajia@gmail.com>
2023-05-24 23:20:31 +0100

    chore: start building game cards

--- src/components/GamePanel.tsx
+++ src/components/GamePanel.tsx
@@ -1,5 +1,6 @@
-import { Text } from '@chakra-ui/react';
+import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
+import GameCard from './GameCard';
 
 const GamePanel = () => {
     const { games, error } = useGames()
@@ -7,9 +8,11 @@ const GamePanel = () => {
   return (
     <>
         {error && <Text>{error}</Text>}
-        <ul>
-            {games.map(game => <li key={game.id}>{game.name}</li>)}
-        </ul>
+        <SimpleGrid columns={{ sm: 1, md: 2, lg: 2, xl: 3 }} spacing={10} padding='10px'>
+            {games.map(game => (
+              <GameCard key={game.id} game={game}/>
+              ))}
+        </SimpleGrid>
     </>
   )
 }
